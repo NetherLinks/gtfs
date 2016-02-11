df6db578b5c4e35fb3f949cfbbf622cf5e0478df
NetherLinks <netherlinks@netherlinks.me>
2016-02-11 03:23:34 -0800

    Support bikes_allowed in Trip

--- spec/gtfs/trip_spec.rb
+++ spec/gtfs/trip_spec.rb
@@ -2,9 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe GTFS::Trip do
   describe 'Trip.parse_trips' do
-    let(:header_line) {"route_id,service_id,trip_id,trip_headsign,direction_id,block_id,shape_id\n"}
+    let(:header_line) {"route_id,service_id,trip_id,trip_headsign,direction_id,block_id,shape_id,wheelchair_accessible,bikes_allowed\n"}
     let(:invalid_header_line) {",,,,direction_id,block_id,shape_id\n"}
-    let(:valid_line) {"4679,1,982394,1 FT McHENRY,0,189021,59135\n"}
+    let(:valid_line) {"4679,1,982394,1 FT McHENRY,0,189021,59135,1,1\n"}
     let(:invalid_line) {",1,,1 FT McHENRY,,189021,\n"}
 
     subject {GTFS::Trip.parse_trips(source_text, opts)}
@@ -12,5 +12,3 @@ describe GTFS::Trip do
     include_examples 'models'
   end
 end
-
-
